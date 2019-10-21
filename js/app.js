90fea0a7a53d2b4a678fd817fd381cb6fe04cb8c
Robin Edmunds <r.edmunds@live.co.uk>
2019-10-21 22:09:23 +0100

    Add addDummySections() function to js/app.js

--- js/app.js
+++ js/app.js
@@ -47,6 +47,36 @@
  *
 */
 
+// Build dummy sections
+
+function addDummySections() {
+  // add dummy sections to the main element
+  const docFragment = document.createDocumentFragment();
+
+  for (let i = 1; i <= 6; i++) {
+    const newSectionElement = document.createElement("section");
+    newSectionElement.innerHTML = (
+      `
+      <div class="landing__container">
+        <h2>Section ${i}</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fermentum metus faucibus lectus pharetra dapibus. Suspendisse potenti. Aenean aliquam elementum mi, ac euismod augue. Donec eget lacinia ex. Phasellus imperdiet porta orci eget mollis. Sed convallis sollicitudin mauris ac tincidunt. Donec bibendum, nulla eget bibendum consectetur, sem nisi aliquam leo, ut pulvinar quam nunc eu augue. Pellentesque maximus imperdiet elit a pharetra. Duis lectus mi, aliquam in mi quis, aliquam porttitor lacus. Morbi a tincidunt felis. Sed leo nunc, pharetra et elementum non, faucibus vitae elit. Integer nec libero venenatis libero ultricies molestie semper in tellus. Sed congue et odio sed euismod.</p>
+
+        <p>Aliquam a convallis justo. Vivamus venenatis, erat eget pulvinar gravida, ipsum lacus aliquet velit, vel luctus diam ipsum a diam. Cras eu tincidunt arcu, vitae rhoncus purus. Vestibulum fermentum consectetur porttitor. Suspendisse imperdiet porttitor tortor, eget elementum tortor mollis non.</p>
+      </div>
+      `
+    );
+    newSectionElement.id = `section${i}`;
+    newSectionElement.setAttribute("data-nav", `Section ${i}`);
+    docFragment.appendChild(newSectionElement);
+  }
+
+  const main = document.querySelector("main");
+  main.appendChild(docFragment);
+}
+
+addDummySections();
+
+
 // Build menu
 
 function addDummyLinks() {
