4765820525bc3ae6598fff34d9ed1425a2be16e8
Robin Edmunds <r.edmunds@live.co.uk>
2019-10-21 21:05:15 +0100

    Add addDummyLinks() function to js/app.js

--- js/app.js
+++ js/app.js
@@ -1,36 +1,35 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
 
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
-
 
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
@@ -45,13 +44,27 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
+
+function addDummyLinks() {
+  // add dummy li links to nav element
+  const docFragment = document.createDocumentFragment();
+
+  for (let i = 1; i <= 6; i++) {
+    const newNavItem = document.createElement("li");
+    newNavItem.innerHTML = `<a href="#section${i}" class="menu__link">Section ${i}</a>`
+    docFragment.appendChild(newNavItem);
+  }
+
+  const nav = document.querySelector("ul#navbar__list");
+  nav.appendChild(docFragment);
+}
+
+addDummyLinks();
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
